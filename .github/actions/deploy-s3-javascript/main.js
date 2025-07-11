43842542c78a23a33238e359580b527db505b25e
ManojKumarKalyanapu <manojkumarkalyanapu777@gmail.com>
2025-07-11 16:58:39 +0530

    deploying site to s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,6 +2,17 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const exec = require("@actions/exec");
 
-async function run() {}
-core.notice("Hello from my custom javascript action");
+async function run() {
+  // get the inputs from the action
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  // upload files to S3
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`); // using aws cli to sync files which is installed in the runner
+
+  core.notice("Hello from my custom Javascript Action");
+}
+
 run();
