9d7dbffd206f15c24786f1f816460cbe992d54b7
ManojKumarKalyanapu <manojkumarkalyanapu777@gmail.com>
2025-07-11 17:13:44 +0530

    output the website url

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,7 +12,8 @@ async function run() {
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`); // using aws cli to sync files which is installed in the runner
 
-  core.notice("Hello from my custom Javascript Action");
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl);
 }
 
 run();
